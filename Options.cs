32d6bdd936848a24fb2daa2bdbede4f33fa1353a
Michael Matus <michael@matus.at>
2020-01-29 22:17:49 +0100

    Initial check-in of module Nmm2Bcr

---
+++ Options.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Nmm2Bcr
+{
+    class Options
+    {
+        [Option('c', "channel", DefaultValue = "-LZ+AZ", HelpText = "Channel to export.")]
+        public string ChannelSymbol { get; set; }
+
+        [Option('s', "scan", DefaultValue = 0, HelpText = "Scan index for multiscan files.")]
+        public int ScanIndex { get; set; }
+
+        [Option('r', "reference", DefaultValue = 1, HelpText = "Mode # of height referencing.")]
+        public int ReferenceMode { get; set; }
+
+        [Option('b', "bias", DefaultValue = 0.0, HelpText = "bias value [um] to be subtracted.")]
+        public double Bias { get; set; }
+
+        [Option('q', "quiet", HelpText = "Quiet mode. No screen output (except for errors).")]
+        public bool BeQuiet { get; set; }
+
+        [Option("iso", HelpText = "Output file ISO 25178-71:2012 complient.")]
+        public bool IsoFormat { get; set; }
+
+        [Option("heydemann", HelpText = "Perform Heydemann correction.")]
+        public bool Heydemann { get; set; }
+
+        [Option("back", HelpText = "Use backtrace profile (when present).")]
+        public bool UseBack { get; set; }
+
+        [Option("both", HelpText = "Mean of forward and backtrace profile (when present).")]
+        public bool UseBoth { get; set; }
+
+        [Option("diff", HelpText = "Difference (forward-backtrace) profile (when present).")]
+        public bool UseDiff { get; set; }
+
+        [Option("relaxed", HelpText = "Allow large (>65535) field dimension")]
+        public bool Relaxed { get; set; }
+
+        [Option("profile", DefaultValue = 0, HelpText = "Extract single profile.")]
+        public int ProfileIndex { get; set; }
+
+
+        [ValueList(typeof(List<string>), MaximumElements = 2)]
+        public IList<string> ListOfFileNames { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            string AppName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            string AppVer = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            HelpText help = new HelpText
+            {
+                Heading = new HeadingInfo(AppName, "version " + AppVer),
+                Copyright = new CopyrightInfo("Michael Matus", 2015),
+                AdditionalNewLineAfterOption = false,
+                AddDashesToOption = true
+            };
+            string sPre = "Program to convert scanning files by SIOS NMM-1 to BCR or ISO 25178-71:2012 raster data format. " +
+                "The quadruple of files (dat ind dsc pos) are analyzed to obtain the required parameters. " +
+                "A rudimentary data processing is possible via the -r option.";
+            help.AddPreOptionsLine(sPre);
+            help.AddPreOptionsLine("");
+            help.AddPreOptionsLine("Usage: " + AppName + " filename1 [filename2] [options]");
+            help.AddPostOptionsLine("");
+            help.AddPostOptionsLine("Supported values for -r: 1=min 2=max 3=avarage 4=mid 5=bias 6=first 7=last 8=center 9=linear 10=LSQ");
+
+            help.AddOptions(this);
+
+            return help;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿using Bev.IO.BcrWriter;
+using Bev.IO.NmmReader;
+using Bev.IO.NmmReader.scan_mode;
+using Bev.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nmm2Bcr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // parse command line arguments
+            var options = new Options();
+            if (!CommandLine.Parser.Default.ParseArgumentsStrict(args, options))
+                Console.WriteLine("*** ParseArgumentsStrict returned false");
+
+            // consume the verbosity option
+            if (options.BeQuiet == true)
+            {
+                ConsoleUI.BeSilent();
+            }
+            else
+            {
+                ConsoleUI.BeVerbatim();
+            }
+
+            // print a welcome message
+            ConsoleUI.Welcome();
+
+            // get the filename(s)
+            string[] fileNames = options.ListOfFileNames.ToArray();
+            if (fileNames.Length == 0)
+                ConsoleUI.ErrorExit("Missing input file!", 1);
+
+            // here all the scan data is read
+            ConsoleUI.StartOperation("Reading and evaluating files");
+            NmmFileName nmmFileNameObject = new NmmFileName(fileNames[0]);
+            int scanIndex = options.ScanIndex;
+            nmmFileNameObject.SetScanIndex(scanIndex);
+            NmmScanData theData = new NmmScanData(nmmFileNameObject);
+            ConsoleUI.Done();
+
+            // some checks of the provided CLA options
+            if (options.ProfileIndex < 0)
+            {
+                options.ProfileIndex = 0;
+            }
+            if (options.ProfileIndex > theData.MetaData.NumberOfProfiles)
+            {
+                options.ProfileIndex = theData.MetaData.NumberOfProfiles;
+            }
+
+            TopographyProcessType topographyProcessType = TopographyProcessType.ForwardOnly;
+            if (options.UseBack)
+            {
+                topographyProcessType = TopographyProcessType.BackwardOnly;
+            }
+            if (options.UseBoth)
+            {
+                topographyProcessType = TopographyProcessType.Average;
+            }
+            if (options.UseDiff)
+            {
+                topographyProcessType = TopographyProcessType.Difference;
+            }
+
+
+            // now we can start to sort and format everything we need
+
+            BcrWriter bcr = new BcrWriter();
+            bcr.Relaxed = options.Relaxed;
+            // ISO 25178-71 file header
+            bcr.CreationDate = theData.MetaData.CreationDate;
+            bcr.ManufacurerId = theData.MetaData.InstrumentIdentifier;
+            bcr.NumberOfPointsPerProfile = theData.MetaData.NumberOfDataPoints;
+            bcr.NumberOfProfiles = theData.MetaData.NumberOfProfiles; // TODO option dependent
+            bcr.XScale = theData.MetaData.ScanFieldDeltaX;
+            bcr.YScale = theData.MetaData.ScanFieldDeltaY;
+
+            // read actual topography data
+            double[] rawData = theData.ExtractProfile(options.ChannelSymbol, options.ProfileIndex, topographyProcessType);
+
+            // level data 
+            DataLeveling levelObject; 
+            if(options.ProfileIndex == 0)
+                levelObject = new DataLeveling(rawData, theData.MetaData.NumberOfDataPoints, theData.MetaData.NumberOfProfiles);
+            else
+                levelObject = new DataLeveling(rawData, theData.MetaData.NumberOfDataPoints);
+            double[] topographyData = levelObject.LevelData(ReferenceTo.Lsq);
+
+            // ISO 25178-71 main section
+            bcr.PrepareMainSection(topographyData);
+
+            // ISO 25178-71 file trailer
+            // generate a dictionary with all relevant metadata
+            Dictionary<string, string> bcrMetaData = new Dictionary<string, string>();
+            bcrMetaData.Add("InputFile", theData.MetaData.BaseFileName);
+            bcrMetaData.Add("ConvertedBy", $"{ConsoleUI.Title} version {ConsoleUI.Version}");
+            bcrMetaData.Add("OperatorName", theData.MetaData.User);
+            bcrMetaData.Add("Organisation", theData.MetaData.Organisation);
+            bcrMetaData.Add("SampleIdentifier", theData.MetaData.SampleIdentifier);
+            bcrMetaData.Add("SampleSpecies", theData.MetaData.SampleSpecies);
+            bcrMetaData.Add("SampleSpecification", theData.MetaData.SampleSpecification);
+            bcrMetaData.Add("SPMtechnique", theData.MetaData.SpmTechnique);
+            bcrMetaData.Add("Probe", theData.MetaData.ProbeDesignation);
+            bcrMetaData.Add("ZAxisSource", options.ChannelSymbol);
+            bcrMetaData.Add("Trace", topographyProcessType.ToString());
+            if (options.ProfileIndex != 0)
+            {
+                bcrMetaData.Add("NumProfiles", $"{theData.MetaData.NumberOfProfiles}");
+                bcrMetaData.Add("ExtractedProfile", $"{options.ProfileIndex}");
+            }
+            if (theData.MetaData.NumberOfScans>1)
+            {
+                bcrMetaData.Add("NumberOfScans", $"{theData.MetaData.NumberOfScans}");
+                bcrMetaData.Add("Scan",$"{theData.MetaData.ScanIndex}");
+            }
+            bcrMetaData.Add("ReferenceDatum", levelObject.LevelModeDescription);
+            bcrMetaData.Add("EnvironmentMode", theData.MetaData.EnvironmentMode);
+            bcrMetaData.Add("SampleTemperature", $"{theData.MetaData.SampleTemperature:F3} oC");
+            bcrMetaData.Add("AirTemperature", $"{theData.MetaData.AirTemperature:F3} oC");
+            bcrMetaData.Add("AirPressure", $"{theData.MetaData.BarometricPressure:F0} Pa");
+            bcrMetaData.Add("AirHumidity", $"{theData.MetaData.RelativeHumidity:F1} %");
+            bcrMetaData.Add("TemperatureGradient", $"{theData.MetaData.AirTemperatureGradient:F3} oC");
+            bcrMetaData.Add("TemperatureRange", $"{theData.MetaData.AirTemperatureDrift:F3} oC");
+            bcrMetaData.Add("ScanSpeed", $"{theData.MetaData.ScanSpeed} um/s");
+            bcrMetaData.Add("ScanAngle", $"{theData.MetaData.ScanFieldRotation:F3} grad");
+            //bcrMetaData.Add("ScanXcenter", $" mm");
+            //bcrMetaData.Add("ScanYcenter", $" mm");
+
+            bcr.PrepareTrailerSection(bcrMetaData);
+
+            // now generate output
+            string outFileName;
+            if(fileNames.Length >=2)
+            {
+                outFileName = fileNames[1];
+            }
+            else
+            {
+                outFileName = nmmFileNameObject.GetFreeFileNameWithIndex("sdf");
+            }
+
+            ConsoleUI.WritingFile(outFileName);
+            if(!bcr.WriteToFile(outFileName))
+            {
+                ConsoleUI.Abort();
+                ConsoleUI.ErrorExit("!could not write file", 1);
+            }
+            ConsoleUI.Done();
+
+        }
+
+    }
+}
